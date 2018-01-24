d9c9300c20f12b1bf5ce898bd4403c7dacaa5eec
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-01-24 16:55:23 +0530

    Update SET1_3.C

--- SET1_3.C
+++ SET1_3.C
@@ -4,23 +4,9 @@ int main()
 char ch;
 printf("Enter the character to check Vowel or Consonant");
 scanf("%c",&ch);
-switch(ch)
-{
-case 1 'a':
-case 1 'A':
-case 1 'e':
-case 1 'E':
-case 1 'i':
-case 1 'I':
-case 1 'o':
-case 1 'O':
-case 1 'u':
-case 1 'U':
-printf("%c is a Vowel",ch);
-break;
-default:
-printf("%c is a consonant",ch);
-}
+if(ch=='a',ch=='A',ch=='e',ch=='E',ch=='i',ch=='I',ch=='o',ch=='O',ch=='u',ch=='U')
+printf("the character is a Vowel");
+else 
+printf("the character is a Consonant");
 return 0;
 }
-
