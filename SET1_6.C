1359a110ec631724193c7d7cac22720fcd7593c7
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-01-29 13:51:23 +0530

    Leap year or not
    To check Leap year or not

---
+++ SET1_6.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the Year");
+scanf("%d",&a);
+if(a%4==0)
+printf("\nThe given year is leap year");
+else 
+printf("\nThe given year is not a leap year");
+return 0;
+}
