e300e3109422276701e854341d158e7d22cba1fa
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-01-29 13:19:47 +0530

    Character or not
    To check whether a Character or not

---
+++ SET1_4.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("'%c' is alphabet.", ch);
+    }
+    else 
+    {
+        printf("'%c' is not a character.", ch);
+    }
+
+    return 0;
+}
