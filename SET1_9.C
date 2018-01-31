9a86c42664516c3a6628539db2a2f83f7b270509
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-01-31 14:13:39 +0530

    Update SET1_9.C

--- SET1_9.C
+++ SET1_9.C
@@ -2,4 +2,20 @@
 #include<conio.h>
 void main()
 {
-int i,num,sum=0;
+int i,j,num,num1,sum=0;
+  printf("Enter the integers \n");
+    scanf ("%d%d", &num,&num1);
+  printf("\n");
+    for(j=1;j<=num;j++)
+    {
+      printf("%d\t",j);
+    }
+ 
+    for (i = 1; i <= num1; i++)
+    {
+        sum = sum + i;
+    }
+    printf ("\nSum of %d number of the integers is = %d\n", num1, sum);
+    getch();
+}
+
