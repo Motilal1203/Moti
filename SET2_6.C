160b72080ce422a4e847dc911cbc542bee8f78ed
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-02-05 13:47:36 +0530

    Update SET2_6.C

--- SET2_6.C
+++ SET2_6.C
@@ -2,24 +2,24 @@
 
 void main()
 { 
-    int n,i,r,t;
+    int j,i,r,t;
     scanf("%d",&n);
     scanf("%d",&r);
-    n++;
-    while(n<r)
+    j++;
+    while(j<r)
     {   t=0;
-        for(i=2;i<n/2;i++)
+        for(i=2;i<j/2;i++)
         {
-            if(n%i==0)
+            if(j%i==0)
             {
                 t=1;
                 break;
             }
         }
         if(t==0)
-        printf("%d ",n);
+        printf("%d",j);
         
-        n++;
+        j++;
     }
    
 }
