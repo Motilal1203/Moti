a720d236fcdb00d468127cc29d01666938ea2c31
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-02-05 14:02:06 +0530

    Update SET2_7.C

--- SET2_7.C
+++ SET2_7.C
@@ -1,1 +1,3 @@
-#include<stdio.h> #include<conio.h>  void main() {     int m,s=0,r,t;     clrscr();     printf("Enter a three digit number");     scanf("%d",&m);     t=m;     while(m>0)    {              r=m%10;      s=s+(r*r*r);      m=m/10;    }                  if(s==t)             printf("\n %d is an armstrong number",t);             else               printf("\n %d is not an armstrong number",t);  getch(); }
+#include<stdio.h>
+#include<conio.h> 
+void main() {     int m,s=0,r,t;     clrscr();     printf("Enter a three digit number");     scanf("%d",&m);     t=m;     while(m>0)    {              r=m%10;      s=s+(r*r*r);      m=m/10;    }                  if(s==t)             printf("\n %d is an armstrong number",t);             else               printf("\n %d is not an armstrong number",t);  getch(); }
