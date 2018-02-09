8534132defff5771d4717c73e8e923702b898775
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-02-09 16:20:31 +0530

    Create SET4_1.C

---
+++ SET4_1.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() 
+{
+	char a[50];
+	int i,c=0;
+	printf("enter the sentence:");
+	scanf("%[^\n]s",a);
+	for(i=0;a[i]!='\0';i++)
+	{
+		c=c+1;
+	}
+	for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]==' ')
+		{
+			c=c-1;
+		}
+	}
+	printf("\n%d",c);
+	return 0;
+}
