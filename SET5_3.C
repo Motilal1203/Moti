60939918ea2479821da874413f6b9da921191bea
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-02-19 13:22:05 +0530

    Create SET5_3.C

---
+++ SET5_3.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s1[50],s2[50];
+printf("Enter the two string : ");
+scanf("%s%s",&s1,&s2);
+printf("\n The concatenated string is :%s%s",s1,s2);
+getch();
+}
