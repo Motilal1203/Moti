c71b3500a2d5474ee0503d03fc40fa4b775cd185
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-02-19 13:17:30 +0530

    Update SET5_2.C

--- SET5_2.C
+++ SET5_2.C
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char s1[50],s2[50];
+int a,b;
+printf("enter the two strings : \n");
+scanf("%s%s",&s1,&s2);
+a=strlen(s1);
+b=strlen(s2);
+if(a>=b)
+{
+printf("%s",s1);
+}
+else
+{
+printf("%s",s2);
+}
+getch();
+}
