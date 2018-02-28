5c80748750401cb0b56afc1cb384d920ff320c6c
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-02-28 14:24:41 +0530

    Update SET6_8.C

--- SET6_8.C
+++ SET6_8.C
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int c,d;
-scanf("%d,%d",&c,&d);
-c=c^d;
-d=c^d;
-c=c^d;
-printf("After swapping:%d,%d",c,d);
+int c,e;
+scanf("%d,%d",&c,&e);
+c=c^e;
+e=c^e;
+c=c^e;
+printf("After swapping:%d,%d",c,e);
 return 0;
 }
