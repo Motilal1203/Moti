d03a277b20b95adb7c8158d4dbf07572b5b70335
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-03-05 13:52:13 +0530

    Create SET8_3.C

---
+++ SET8_3.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,i,d=0;
+scanf("%d",&a);
+printf("range :");
+scanf("%d%d",&b,&c);
+for(i=b;i<=c;i++)
+{
+if(i==a)
+{
+d=1;
+}
+else
+{
+}
+}
+if(d==1)
+{
+    printf("yes");
+}
+else
+{
+    printf("no");
+}
+getch();
+}
