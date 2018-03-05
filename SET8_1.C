55ba11f0bf88f7921769ce5374bd489b3c49c6a6
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-03-05 13:28:14 +0530

    Create SET8_1.C

---
+++ SET8_1.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[1000],b[1000];
+int c;
+scanf("%s",a);
+strcpy(b,a);
+strrev(a);
+c=strcmp(a,b);
+if(c==0)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+getch();
+}
