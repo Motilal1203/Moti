330902d9fb9f0bec46eed150218e7302bb48d661
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-03-19 15:58:35 +0530

    Create SET9_8.C

---
+++ SET9_8.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,i,g,z;
+	printf("Enter the Two Numbers:");
+	scanf("%d%d",&a,&b);	
+	for(i=1;i<=a&&i<=b;i++)
+	{
+		if(a%i==0&&b%i==0)
+		{
+			g=i;
+		}
+	}
+	z=(a*b)/g;
+	printf("%d",l);
+}
