fd82f42bf4dbc72d0c8df3c394c021706162e4fb
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-03-23 15:45:46 +0530

    Update SET10_4.C

--- SET10_4.C
+++ SET10_4.C
@@ -1,1 +1,14 @@
 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a[10],n,k,i;
+ printf("enter k and n value:");
+ scanf("%d%d",&n,&k);
+ for(i=0;i<n;i++)
+ {
+   scanf("%d",a[k-1]);
+   }
+   getch();
+   }
