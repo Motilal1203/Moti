16306823bea82af32463b30f18b808cae6822a76
Motilal1203 <35687707+Motilal1203@users.noreply.github.com>
2018-03-26 16:10:50 +0530

    Create SET11_8.C

---
+++ SET11_8.C
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main()
+{
+int a,d,n;
+clrscr();
+printf("enter the first term:");
+scanf("%d",&a);
+printf("enter difference:");
+scanf("%d",&d);
+printf("enter no of terms:");
+scanf("%d",&n);
+int sum=0,i;
+for(i=0;i<n;i++)
+{
+sum=sum+(a+i*d);
+}
+printf("%d",sum);
+return 0;
+}
